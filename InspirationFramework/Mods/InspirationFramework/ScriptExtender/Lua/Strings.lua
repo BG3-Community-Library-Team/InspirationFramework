dffdd2ec2984b34886557e35920cc1ebac1b0474
NellsRelo <NellsRelo@gmail.com>
2024-01-29 15:55:58 -0800

    Register Background Goal

--- InspirationFramework/Mods/InspirationFramework/ScriptExtender/Lua/Strings.lua
+++ InspirationFramework/Mods/InspirationFramework/ScriptExtender/Lua/Strings.lua
@@ -2,6 +2,8 @@ Strings = {}
 
 Strings.InvalidOperation = "Invalid DB Operation Supplied."
 
-Strings.ERR_DB_GLO_Backgrounds_Players_Exists = "Player already found in DB_GLO_Backgrounds_Players"
+Strings.INFO_DB_GLO_Backgrounds_Players_Exists = "Player already found in DB_GLO_Backgrounds_Players"
 
 Strings.FRAG_SKIPPING = ", Skipping..."
+
+Strings.WARN_DB_GLU_Backgrounds_Goal_Exists = "Attempted to register duplicate Background Goal Name: "
--- InspirationFramework/Mods/InspirationFramework/ScriptExtender/Lua/Utils/GoalUtils.lua
+++ InspirationFramework/Mods/InspirationFramework/ScriptExtender/Lua/Utils/GoalUtils.lua
@@ -1,28 +1,46 @@
 --- Loop through DB_Players and register the background tag for any players that aren't already registered
 function Utils.RegisterBackgroundTag()
   for _, playerID in pairs(Osi.DB_Players[1]) do
-    local player = Ext.Entity.Get(playerID)
-    -- Grabbing the first Background tag, as while multiple tags are technically supported, it gets funky
     if not Conditions.IsInBackgroundPlayersDB(playerID) then
-      Utils.HandleBackgroundsPlayers("Add", playerID, player.BackgroundTag.Tags[1])
+      Utils.HandleBackgroundsPlayers("Add", playerID, Utils.GetBackgroundTag(playerID, "Character", true))
     else
-      CLUtils.Info(Strings.ERR_DB_GLO_Backgrounds_Players_Exists .. Strings.FRAG_SKIPPING)
+      CLUtils.Info(Strings.INFO_DB_GLO_Backgrounds_Players_Exists .. Strings.FRAG_SKIPPING)
     end
   end
 end
 
-function Utils.RegisterBackgroundGoal()
-
+--- Register Background Goal to DB_GLU_Backgrounds_Goal. 
+--- @param backgroundId string UUID of Background
+--- @param goalId string UUID of Background Goal
+--- @param goalString string String name to track goal. Example `Act1_TestGoal`. This should be prefixed with your mod prefix to avoid collisions.
+--- @param global string Value is either `Global` or `GlobalAvatar`. Defaults to `Global`
+function Utils.RegisterBackgroundGoal(backgroundId, goalId, goalString, global)
+  global = global or "Global"
+  local backgroundTagId = Utils.GetBackgroundTag(backgroundId, "Background", true)
+  if not Utils.TruthDB(Utils.HandleBackgroundsGoal("Get", _, goalString, _, _)) then
+    Utils.HandleBackgroundsGoal("Add", backgroundTagId, goalString, goalId, global)
+  else
+    CLUtils.Warn(Strings.WARN_DB_GLU_Backgrounds_Goal_Exists .. goalString)
+  end
 end
+
 -- Osi.DB_GLO_Backgrounds_Goal:Get(backgroundTagId, goalString, goalId)
 
 --- Retrieve the Background Tag from a given Character
---- @param characterId string ID of character. Defaults to Host Character.
+--- @param id string ID of character. Defaults to Host Character.
+--- @param idType string Type of object we're getting the UUID of. Supports `Character` or `Background`. Defaults to `Character`.
 --- @param full boolean Boolean to determine if tag should be prefixed with Tag Name field.
 ---@return string
-function Utils.GetBackgroundTag(characterId, full)
-  characterId = characterId or Osi.GetHostCharacter()
-  local tag = Ext.Entity.Get(characterId).BackgroundTag.Tags[1]
+function Utils.GetBackgroundTag(id, idType, full)
+  idType = idType or "Character"
+  local tag
+  if idType == "Background" then
+    local background = Ext.StaticData.Get(id, "Background")
+    tag = background.Tags[1]
+  else
+    id = id or Osi.GetHostCharacter()
+    tag = Ext.Entity.Get(id).BackgroundTag.Tags[1]
+  end
   if full then
     tag = Ext.StaticData.Get(tag, "Tag").Name .. "_" .. tag
   end
